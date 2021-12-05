a041b9d81e6d235728f251b9e556517f7f3c7bbb
Eugene Korbut <me@mvl.ru>
2021-12-05 10:53:35 +0300

    Day 04, part 2

--- src/day04/main.rs
+++ src/day04/main.rs
@@ -1,9 +1,76 @@
+use std::collections::VecDeque;
 use std::error::Error;
 
 const SIZE: usize = 5;
 
 fn index(row: usize, col: usize) -> usize {
     row * SIZE + col
+}
+
+struct Game {
+    numbers: VecDeque<i32>,
+    cards: Vec<(Card, bool)>,
+    keep: Option<i32>,
+}
+
+impl Game {
+    fn parse(input: &str) -> Result<Self, Box<dyn Error>> {
+        let mut parts = input.split("\n\n");
+
+        let numbers_raw = parts.nth(0).ok_or("invalid input")?;
+        let numbers = numbers_raw
+            .split(",")
+            .map(|p| p.parse::<i32>())
+            .collect::<Result<VecDeque<_>, _>>()?;
+
+        let cards = parts.map(Card::parse).collect::<Result<Vec<_>, _>>()?;
+        let cards = cards.into_iter().map(|c| (c, false)).collect();
+        let keep = None;
+
+        Ok(Self {
+            numbers,
+            cards,
+            keep,
+        })
+    }
+
+    fn find_and_mark(&mut self) -> Option<i32> {
+        self.cards
+            .iter_mut()
+            .filter(|(c, used)| c.ready() && !used)
+            .map(|(c, used)| {
+                *used = true;
+                c.sum_unmarked()
+            })
+            .nth(0)
+    }
+
+    fn mark(&mut self, k: i32) {
+        self.cards.iter_mut().for_each(|(c, _)| c.mark(k));
+    }
+}
+
+impl Iterator for Game {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(v) = self.find_and_mark() {
+            match self.keep {
+                Some(k) => return Some(v * k),
+                None => panic!("Unexpected"),
+            }
+        }
+
+        while let Some(k) = self.numbers.pop_front() {
+            self.keep = Some(k);
+            self.mark(k);
+            if let Some(v) = self.find_and_mark() {
+                return Some(v * k);
+            }
+        }
+
+        None
+    }
 }
 
 #[derive(Debug)]
@@ -59,27 +126,13 @@ impl Card {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let input = std::env::args().nth(1).ok_or("Invalid input")?;
-    let raw = std::fs::read_to_string(input)?;
-    let mut parts = raw.split("\n\n");
+    let filename = std::env::args().nth(1).ok_or("Invalid input")?;
+    let input = std::fs::read_to_string(filename)?;
+    let mut game = Game::parse(&input)?;
 
-    let numbers_raw = parts.nth(0).ok_or("invalid input")?;
-    let numbers = numbers_raw
-        .split(",")
-        .map(|p| p.parse::<i32>())
-        .collect::<Result<Vec<_>, _>>()?;
+    let result_a = game.next().ok_or_else(|| "Result A not found!")?;
+    let result_b = game.last().ok_or_else(|| "Result B not found!")?;
+    println!("Task A: {}\nTask B: {}", result_a, result_b);
 
-    let mut cards = parts.map(Card::parse).collect::<Result<Vec<_>, _>>()?;
-    let founed = numbers
-        .iter()
-        .filter_map(|n| {
-            cards.iter_mut().for_each(|c| c.mark(*n));
-            let ready = cards.iter().find(|c| c.ready());
-            ready.map(|c| c.sum_unmarked()).map(|sum| sum * n)
-        })
-        .nth(0)
-        .ok_or_else(|| "Result not found!")?;
-
-    println!("Task A: {}", founed);
     Ok(())
 }
