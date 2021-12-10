444889b63c958a5171cd728dc375cef38bd1e175
Eugene Korbut <me@mvl.ru>
2021-12-10 12:37:39 +0300

    Day 10, part 2

--- src/day10/main.rs
+++ src/day10/main.rs
@@ -1,6 +1,11 @@
 use std::{collections::VecDeque, error::Error};
 
-fn find_wrong(input: &str) -> Option<char> {
+enum Info {
+    Wrong(char),
+    Missing(Vec<char>),
+}
+
+fn parse(input: &str) -> Info {
     let mut buffer = VecDeque::new();
     for char in input.chars() {
         match char {
@@ -8,39 +13,76 @@ fn find_wrong(input: &str) -> Option<char> {
             '}' => {
                 if let Some('{') = buffer.pop_back() {
                 } else {
-                    return Some(char);
+                    return Info::Wrong(char);
                 }
             }
             '>' => {
                 if let Some('<') = buffer.pop_back() {
                 } else {
-                    return Some(char);
+                    return Info::Wrong(char);
                 }
             }
             ']' => {
                 if let Some('[') = buffer.pop_back() {
                 } else {
-                    return Some(char);
+                    return Info::Wrong(char);
                 }
             }
             ')' => {
                 if let Some('(') = buffer.pop_back() {
                 } else {
-                    return Some(char);
+                    return Info::Wrong(char);
                 }
             }
             _ => panic!("Invalid char: {}", char),
         }
     }
-    None
+
+    let mut missing = vec![];
+    while let Some(c) = buffer.pop_back() {
+        match c {
+            '{' => missing.push('}'),
+            '<' => missing.push('>'),
+            '(' => missing.push(')'),
+            '[' => missing.push(']'),
+            _ => panic!("Invalid char: {}", c),
+        }
+    }
+
+    Info::Missing(missing)
+}
+
+fn score_missing(input: &char) -> i64 {
+    match input {
+        ')' => 1,
+        ']' => 2,
+        '}' => 3,
+        '>' => 4,
+        _ => panic!("Invalid char: {}", input),
+    }
+}
+
+fn calculate_missing(input: &[i64]) -> i64 {
+    input.iter().fold(0, |a, e| a * 5 + e)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let filename = std::env::args().nth(1).ok_or("Invalid input")?;
     let raw = std::fs::read_to_string(filename)?;
+    let parsed = raw.lines().map(parse).collect::<Vec<_>>();
 
-    let chars = raw.lines().filter_map(find_wrong).collect::<Vec<_>>();
-    let score: isize = chars
+    let wrong = parsed
+        .iter()
+        .filter_map(|info| {
+            if let Info::Wrong(c) = info {
+                Some(c)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let result_a: isize = wrong
         .iter()
         .map(|c| match c {
             ')' => 3,
@@ -51,7 +93,25 @@ fn main() -> Result<(), Box<dyn Error>> {
         })
         .sum();
 
-    dbg!(score);
+    let mut missing = parsed
+        .iter()
+        .filter_map(|info| {
+            if let Info::Missing(m) = info {
+                let scored = m.iter().map(score_missing).collect::<Vec<_>>();
+                let total = calculate_missing(&scored);
+                Some(total)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    missing.sort();
+
+    let idx = (missing.len() - 1) / 2;
+    let middle = missing[idx];
+
+    println!("Task A: {}\nTask B: {}", result_a, middle);
 
     Ok(())
 }
