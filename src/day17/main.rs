e445c031e3715dea3044373155fe9a7d4ed0e8d9
Eugene Korbut <me@mvl.ru>
2021-12-17 10:42:21 +0300

    Placeholder for day 17

---
+++ src/day17/main.rs
@@ -0,0 +1,12 @@
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let filename = std::env::args().nth(1).ok_or("Invalid input")?;
+    let input = std::fs::read_to_string(filename)?;
+    let result_a = 0;
+    let result_b = 0;
+
+    println!("Task A: {}\nTask B: {}", result_a, result_b);
+
+    Ok(())
+}
