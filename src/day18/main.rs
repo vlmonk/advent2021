ecf5434a16b090c66fe3b3149f142e097d511f17
Eugene Korbut <me@mvl.ru>
2021-12-18 19:46:25 +0300

    Placeholder for day 18

---
+++ src/day18/main.rs
@@ -0,0 +1,135 @@
+use std::error::Error;
+
+#[derive(Debug, PartialEq)]
+enum Token {
+    Open,
+    Close,
+    Comma,
+    Num(i32),
+}
+
+struct Tokenizer<'a> {
+    position: usize,
+    input: &'a str,
+}
+
+impl<'a> Tokenizer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self { input, position: 0 }
+    }
+}
+
+impl<'a> Iterator for Tokenizer<'a> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut chars = self.input.get(self.position..)?.chars();
+        while let Some(c) = chars.next() {
+            match c {
+                '[' => {
+                    self.position += c.len_utf8();
+                    return Some(Token::Open);
+                }
+                ']' => {
+                    self.position += c.len_utf8();
+                    return Some(Token::Close);
+                }
+                ',' => {
+                    self.position += c.len_utf8();
+                    return Some(Token::Comma);
+                }
+                c if c.is_digit(10) => {
+                    let next_chars_len: usize = chars
+                        .take_while(|c| c.is_digit(10))
+                        .map(|c| c.len_utf8())
+                        .sum();
+                    let len = next_chars_len + c.len_utf8();
+                    let slice = self.input.get(self.position..self.position + len)?;
+                    let value = slice.parse::<i32>().ok()?;
+                    self.position += len;
+                    return Some(Token::Num(value));
+                }
+                c => {
+                    self.position += c.len_utf8();
+                }
+            }
+        }
+        None
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Number {
+    Single(i32),
+    Pair(Box<Number>, Box<Number>),
+}
+
+impl Number {
+    pub fn parse(input: &str) -> Option<Self> {
+        None
+    }
+
+    pub fn single(input: i32) -> Self {
+        Self::Single(input)
+    }
+
+    pub fn pair(a: Number, b: Number) -> Self {
+        let a = Box::new(a);
+        let b = Box::new(b);
+        Self::Pair(a, b)
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let filename = std::env::args().nth(1).ok_or("Invalid input")?;
+    let input = std::fs::read_to_string(filename)?;
+    dbg!(&input);
+    let mut tokenizer = Tokenizer::new(&input);
+    dbg!(tokenizer.next());
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    // #[test]
+    // fn test_simple() {
+    //     let input = "[1,2]";
+    //     let expected = Number::pair(Number::single(1), Number::Single(2));
+    //     let result = Number::parse(input).unwrap();
+    //     assert_eq!(result, expected);
+    // }
+
+    #[test]
+    fn test_tokenizer() {
+        let input = "[],";
+        let mut tokenizer = Tokenizer::new(input);
+        assert_eq!(tokenizer.next(), Some(Token::Open));
+        assert_eq!(tokenizer.next(), Some(Token::Close));
+        assert_eq!(tokenizer.next(), Some(Token::Comma));
+        assert_eq!(tokenizer.next(), None);
+    }
+
+    #[test]
+    fn test_tokenizer_num() {
+        let input = "9923,[119";
+        let mut tokenizer = Tokenizer::new(input);
+        assert_eq!(tokenizer.next(), Some(Token::Num(9923)));
+        assert_eq!(tokenizer.next(), Some(Token::Comma));
+        assert_eq!(tokenizer.next(), Some(Token::Open));
+        assert_eq!(tokenizer.next(), Some(Token::Num(119)));
+        assert_eq!(tokenizer.next(), None);
+    }
+
+    #[test]
+    fn test_tokenizer_skip_space() {
+        let input = " 999 , 111 ";
+        let mut tokenizer = Tokenizer::new(input);
+        assert_eq!(tokenizer.next(), Some(Token::Num(999)));
+        assert_eq!(tokenizer.next(), Some(Token::Comma));
+        assert_eq!(tokenizer.next(), Some(Token::Num(111)));
+        assert_eq!(tokenizer.next(), None);
+    }
+}
