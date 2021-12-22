ddc224678cbd67ddf45d3809655b98b98f1c388f
Eugene Korbut <me@mvl.ru>
2021-12-22 09:51:45 +0300

    Day 22 placeholder

---
+++ src/day22/main.rs
@@ -0,0 +1,13 @@
+use std::{collections::HashMap, error::Error, fmt::Display};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let filename = std::env::args().nth(1).ok_or("Invalid input")?;
+    let input = std::fs::read_to_string(filename)?;
+    dbg!(input);
+
+    let result_a = 0;
+    let result_b = 0;
+    println!("Task A: {}, Task B: {}", result_a, result_b);
+
+    Ok(())
+}
