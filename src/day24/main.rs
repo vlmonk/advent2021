105465553400488ef7f232cf45610dfdd0c62c4b
Eugene Korbut <me@mvl.ru>
2021-12-26 01:33:47 +0300

    Impl computer

--- src/day24/main.rs
+++ src/day24/main.rs
@@ -23,14 +23,14 @@ impl Reg {
 #[derive(Debug)]
 enum Src {
     Reg(Reg),
-    Value(i32),
+    Value(i64),
 }
 
 impl Src {
     pub fn parse(input: &str) -> Option<Self> {
         Reg::parse(input)
             .map(|r| Self::Reg(r))
-            .or_else(|| input.parse::<i32>().ok().map(|value| Self::Value(value)))
+            .or_else(|| input.parse::<i64>().ok().map(|value| Self::Value(value)))
     }
 }
 
@@ -64,6 +64,99 @@ impl Op {
     }
 }
 
+#[derive(Debug)]
+struct Computer {
+    w: i64,
+    x: i64,
+    y: i64,
+    z: i64,
+}
+
+impl Computer {
+    pub fn new() -> Self {
+        Self {
+            w: 0,
+            x: 0,
+            y: 0,
+            z: 0,
+        }
+    }
+
+    pub fn run(&mut self, programm: &[Op], input: &[i64]) {
+        let mut input = input.iter();
+
+        for op in programm {
+            match op {
+                Op::Inp(reg) => {
+                    let src = input.next().expect("Empty input");
+                    let target = self.reg(reg);
+                    *target = *src;
+                }
+
+                Op::Add(reg, src) => {
+                    let src = self.src(src);
+                    let target = self.reg(reg);
+                    *target += src;
+                }
+
+                Op::Mul(reg, src) => {
+                    let src = self.src(src);
+                    let target = self.reg(reg);
+                    *target *= src;
+                }
+
+                Op::Div(reg, src) => {
+                    let src = self.src(src);
+                    let target = self.reg(reg);
+                    *target /= src;
+                }
+
+                Op::Mod(reg, src) => {
+                    let src = self.src(src);
+                    let target = self.reg(reg);
+                    *target %= src;
+                }
+
+                Op::Eql(reg, src) => {
+                    let src = self.src(src);
+                    let target = self.reg(reg);
+
+                    if *target == src {
+                        *target = 1
+                    } else {
+                        *target = 0
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn reg(&mut self, reg: &Reg) -> &mut i64 {
+        match reg {
+            Reg::W => &mut self.w,
+            Reg::X => &mut self.x,
+            Reg::Y => &mut self.y,
+            Reg::Z => &mut self.z,
+        }
+    }
+
+    pub fn value(&self, reg: &Reg) -> i64 {
+        match reg {
+            Reg::W => self.w,
+            Reg::X => self.x,
+            Reg::Y => self.y,
+            Reg::Z => self.z,
+        }
+    }
+
+    pub fn src(&self, src: &Src) -> i64 {
+        match src {
+            Src::Value(val) => *val,
+            Src::Reg(reg) => self.value(reg),
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let filename = std::env::args().nth(1).ok_or("Invalid input")?;
     let input = std::fs::read_to_string(filename)?;
@@ -71,9 +164,13 @@ fn main() -> Result<(), Box<dyn Error>> {
         .lines()
         .map(Op::parse)
         .collect::<Option<Vec<_>>>()
-        .ok_or("Can't parse");
+        .ok_or("Can't parse")?;
 
     dbg!(&commands);
+
+    let mut computer = Computer::new();
+    computer.run(&commands, &[9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]);
+    dbg!(computer);
 
     let result_a = 0;
     let result_b = 0;
